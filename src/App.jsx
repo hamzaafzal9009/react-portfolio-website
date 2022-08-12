8838f0e0ec542905bb6e09a1f4fe77f766481d36
hamzaafzal <hamza@syncitgroup.com>
2022-08-12 15:49:59 +0500

    components created and imported in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import "./index.css"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import About from "./components/about/About"
+import Experience from "./components/experience/Experience"
+import Contact from "./components/contact/Contact"
+import Nav from "./components/nav/Nav"
+import Portfolio from "./components/portfolio/Portfolio"
+import Services from "./components/services/Services"
+import Testimonials from "./components/testimonials/Testimonials"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+     <Header />
+     <Nav />
+     <About />
+     <Experience />
+     <Services />
+     <Portfolio />
+     <Testimonials />
+     <Contact />
+     <Footer />
+
+    </>
   )
 }
 

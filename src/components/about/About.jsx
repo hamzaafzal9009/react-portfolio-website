77eb6b7bd6c062ee62a8e88193939cd5d50e3ce2
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2022-08-24 16:01:12 -0700

    About structure completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,58 @@
 import React from "react";
 
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={ME} alt="..." />
+        </div>
+        <div className="about__content">
+          <article className="about__cards">
+            <div className="about_card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>5+ Years Working</small>
+            </div>
+          </article>
+
+          <article className="about__cards">
+            <div className="about_card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </div>
+          </article>
+          <article className="about__cards">
+            <div className="about_card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>50+ Completed Projects</small>
+            </div>
+          </article>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          nostrum nemo possimus! Animi amet recusandae doloremque reprehenderit
+          tempora adipisci suscipit excepturi eius, nesciunt vero debitis
+          exercitationem maiores a, sit beatae.
+        </p>
+
+        <a href="#contact" className="btn btn-primary">Let's Talk</a>
+      </div>
+    </section>
+  );
 };
 
 export default About;

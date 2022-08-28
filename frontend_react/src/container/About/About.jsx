45a962d828bb0dcc0e15957851bd9f296076e96f
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2022-08-28 07:59:33 -0700

    about settings

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,31 +3,19 @@ import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./about.scss";
-
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am good at web developer",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Frontend Development",
-    description: "I am a web designer",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Backend Development",
-    description: "I am a web developer",
-    imgUrl: images.about03,
-  },
-  {
-    title: "UI/UX",
-    description: "I am a web developer",
-    imgUrl: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -41,10 +29,10 @@ const About = () => {
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: "tween" }}
-            className="app__profiles-item"
+            className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
